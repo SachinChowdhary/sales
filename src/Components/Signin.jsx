47cdd437d88849ff59ff2a79026a889b3da4fe37
ksachin3025@gmail.com <sachin@edvenswatech.com>
2025-04-28 17:15:39 +0530

    Push the SignUp

--- src/Components/Signin.jsx
+++ src/Components/Signin.jsx
@@ -1,9 +1,9 @@
 import React from "react"; 
-import { useNavigate  } from "react-router-dom";
+import { Link  } from "react-router-dom";
+import Font from "./Font";
 export default function SignIn({settoken}) {
     const[Email, setEmail] = React.useState('')
     const[Password, setPassword] = React.useState('')
-    const navigate = useNavigate()
     React.useEffect(() => {
         const root = document.getElementById("root");
         root.classList.add("white-bg");
@@ -13,41 +13,54 @@ export default function SignIn({settoken}) {
       }, []);
 
     async function login(){
-        const response = await fetch('https://loginsignupbanckend-7.onrender.com/auth/login',{
-                                 headers: {
-                                           'Content-Type': 'application/json',
-                                         },
-                                 method:'POST',
-                                 body: JSON.stringify({
-                                    email:Email,
-                                    password:Password
-                                 })
-                              })
-         const json = await response.json()
-         if('error' in json){
-            alert(json.error)
-         }
-         else{
-           localStorage.setItem('Token',json.access_token)
-           settoken(!!localStorage.getItem('Token'))
-         }
-    }
+      if(Email!=='' && Password!==''){
+            const response = await fetch('https://loginsignupbanckend-7.onrender.com/auth/login',{
+                                   headers: {
+                                        'Content-Type': 'application/json',
+                                           },
+                                   method:'POST',
+                                   body: JSON.stringify({
+                                              email:Email,
+                                              password:Password
+                                              })
+                                   })
+              const json = await response.json()
+            if('error' in json){
+             alert(json.error)
+             }
+           else{
+              localStorage.setItem('Token',json.access_token)
+              settoken(!!localStorage.getItem('Token'))
+             }
+       }
+        
+     }
     return (    
     <div className="flex items-center justify-center m-10">
-        <div className="flex flex-col gap-4 w-full max-w-sm">
+        <div className="flex flex-col gap-4 w-[80%] sm:w-[50%] md:w-[30%]">
+          <Font className='font-medium'>Work Email</Font>
           <input
             type="email"
-            placeholder="Enter Email Address"
-            className="border border-gray-300 focus:border-blue-500 outline-none rounded-md h-10 px-4"
+            placeholder="Work Email"
+            className="border border-gray-300 focus:border-blue-500 outline-none rounded-md h-10 px-4 font-serif placeholder:font-serif"
             onChange={(e)=>setEmail(e.target.value)}
+            value={Email}
           />
+          {
+           Email === "" && <Font className="text-white text-[10px] bg-red-500 px-2 rounded-sm">Work Email must not be empty </Font>
+          }
+          <Font className='font-medium'>Password</Font>
           <input
             type="password"
-            placeholder="Enter Password"
-            className="border border-gray-300 focus:border-blue-500 outline-none rounded-md h-10 px-4"
+            placeholder="Password"
+            className="border border-gray-300 focus:border-blue-500 outline-none rounded-md h-10 px-4 font-serif placeholder:font-serif"
             onChange={(e)=>setPassword(e.target.value)}
           />
-            <button className="bg-blue-500 rounded-md h-8" onClick={()=>{login()}}>Submit</button>
+             {
+               Password==="" && <Font className='text-white text-[10px] bg-red-500 px-1 rounded-sm'>Password must not be empty</Font>
+              }
+            <Font>Not a member yet? <Link to='/signup' className="text-blue-500 font-serif">Join</Link></Font>
+            <button className="bg-blue-500 rounded-md h-8 font-serif" onClick={()=>{login()}}>Sign In</button>
         </div>
      </div>
     )
--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -1,9 +1,80 @@
 import React from "react"; 
-
+import { Link } from "react-router-dom";
+import Font from "./Font";
 export default function SignUp() {
+     const[userName, setUserName]=React.useState('')
+     const[Email, setEmail] = React.useState('')
+     const[Password, setPassword] = React.useState('')
+     const[message, setMessage] = React.useState('')
+    const SignUp=async()=>{
+        if(Email!=='' && Password!=="" && userName!==''){
+           const response = await fetch('https://loginsignupbanckend-7.onrender.com/auth/signup',{
+                                 headers:{
+                                         'Content-Type': 'application/json',
+                                        },
+                                 method:'POST',
+                                 body:JSON.stringify({
+                                    username:userName,
+                                    email: Email,
+                                    password: Password
+                                 })
+                                 })
+          const data = await response.json()
+          if(data.error){
+            setMessage(data.error)
+          }
+          else{
+            setMessage(data.message)
+          }
+        }
+    }
+    console.log(message,"jdvn")
+    React.useEffect(()=>{
+    const root = document.getElementById('root')
+    root.classList.add('white-bg')
+    return(()=>{
+        root.classList.remove('white-bg')
+    })
+    },[])
     return (
-        <div>
-            <h1>SignUp</h1>
+        <div className="flex justify-center items-center m-10">
+            <div className="flex flex-col gap-4 w-[80%] sm:w-[50%] md:w-[30%]">
+              <Font className='font-medium'>User Name</Font>
+              <input 
+              type="text" 
+              placeholder="User Name" 
+              className="border border-gray-300 focus:border-blue-500 outline-none rounded-md font-serif h-10 px-4 placeholder:font-serif"
+              onChange={(e)=>setUserName(e.target.value)}
+              />
+              {
+                userName==="" && <Font className='text-white text-[10px] bg-red-500 px-1 rounded-sm'>User Name must not be empty</Font>
+              }
+              <Font className='font-medium'>Work Email</Font>
+              <input 
+              type="email" 
+              placeholder="Work Email" 
+              className="border border-gray-300 font-serif focus:border-blue-500 outline-none rounded-md h-10 px-4 placeholder:font-serif"
+              onChange={(e)=>setEmail(e.target.value)}
+              />
+              {
+               Email==="" && <Font className='text-white text-[10px] bg-red-500 px-1 rounded-sm'>Work Email must not be empty</Font>
+              }
+              <Font className='font-medium'>Password</Font>
+              <input
+              type="password"
+              placeholder="Password"
+              className="border border-gray-300 focus:border-blue-500 outline-none rounded-md h-10 px-4 font-serif placeholder:font-serif"
+              onChange={(e)=>setPassword(e.target.value)}
+             />
+              {
+               Password==="" && <Font className='text-white text-[10px] bg-red-500 px-1 rounded-sm'>Password must not be empty</Font>
+              }
+              <Font>Are you a member? <Link to='/' className="text-blue-500 font-serif">Sign In</Link></Font>
+              <button className="bg-blue-500 rounded-md h-8 font-serif" onClick={()=>SignUp()}>Sign Up</button>
+              {
+                message && <Font className={`${message.includes('exists') ? 'text-red-500' : 'text-blue-500'}`}>{message}</Font>
+              }
+             </div>
         </div>
     )
 }
